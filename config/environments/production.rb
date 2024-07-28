76dfbd2c4fa10813d707dfdfb7eae7b7a2082466
Alex Kearns <alexkearns@users.noreply.github.com>
2024-07-28 15:40:51 +0000

    Add handling of static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{30.days.seconds.to_i}",
+    'X-Lamby-Base64' => '1'
+  }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
